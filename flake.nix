753f145d19f3b1e5ce3d9c7dc69ccaddfc05c5f2
HaeNoe <programmierhaenoe@outlook.de>
2023-05-24 15:10:28 +0200

    add flake inputs

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A configuration all things nix.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs }: { };
+}
