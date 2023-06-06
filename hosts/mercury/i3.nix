5465d45ebc6af162bd2e98ae8926c1b2f4049461
HaeNoe <programmierhaenoe@outlook.de>
2023-06-06 19:03:52 +0200

    add .gitignore

--- hosts/mercury/i3.nix
+++ hosts/mercury/i3.nix
@@ -1,3 +1,8 @@
 { home-manager, userName, ... }: {
+  services.xserver = {
+    enable = true;
+    windowManager.i3.enable = true;
+  };
+
   home-manager.users.${userName}.xsession.windowManager.i3.enable = true;
 }
