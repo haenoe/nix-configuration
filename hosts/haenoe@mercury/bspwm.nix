3fb6fbeed0de8955dc998db5aa1d70634ebb28ad
haenoe <programmierhaenoe@outlook.de>
2023-09-30 19:21:33 +0200

    fix(mercury): set right keybinds for refocussing and moving windows

--- hosts/haenoe@mercury/bspwm.nix
+++ hosts/haenoe@mercury/bspwm.nix
@@ -34,7 +34,8 @@
           "alt + Return" = "alacritty";
           "alt + space" = "${pkgs.rofi}/bin/rofi -show drun";
           "alt + q" = "${pkgs.bspwm}/bin/bspc node -c";
-          "alt + {_, shift +} + {h,j,k,l}" = "${pkgs.bspwm}/bin/bspc node -{f,s} {west,east,north,south}";
+	  "alt + {a,o,e,u}" = "${pkgs.bspwm}/bin/bspc desktop -f {1-4}";
+          "alt + {_, shift +} + {h,j,k,l}" = "${pkgs.bspwm}/bin/bspc node -{f,s} {west,south,north,east}";
         };
       };
     };
