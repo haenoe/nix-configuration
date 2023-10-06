617f42c27ec8eed89a23d678a846db5d553b9030
haenoe <programmierhaenoe@outlook.de>
2023-10-06 18:34:03 +0200

    feat(traefik): enable acme certificate generation with cloudflare

--- hosts/haenoe@saturn/services/traefik.nix
+++ hosts/haenoe@saturn/services/traefik.nix
@@ -14,6 +14,14 @@
       "--entrypoints.web.address=:80"
       "--entrypoints.web.http.redirections.entryPoint.to=websecure"
       "--entrypoints.web.http.redirections.entryPoint.scheme=https"
+      "--certificatesresolvers.cfresolver.acme.email=programmierhaenoe@outlook.de"
+      "--certificatesresolvers.cfresolver.acme.storage=/letsencrypt/acme.json"
+      "--certificatesresolvers.cfresolver.acme.dnschallenge=true"
+      "--certificatesresolvers.cfresolver.acme.dnschallenge.resolvers=1.1.1.1:53,8.8.8.8:53"
+      "--certificatesresolvers.cfresolver.acme.dnschallenge.provider=cloudflare"
     ];
+    environment = {
+    };
+    volumes = [ "/run/docker.sock:/var/run/docker.sock:ro" "/etc/traefik/letsencrypt:/letsencrypt" ];
   };
 }
