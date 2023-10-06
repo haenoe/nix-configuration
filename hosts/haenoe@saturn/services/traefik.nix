96c5f59ae0751d0a5e648c4384974829bdb79f4e
haenoe <programmierhaenoe@outlook.de>
2023-10-06 18:34:37 +0200

    feat(traefik): add example whoami service

--- hosts/haenoe@saturn/services/traefik.nix
+++ hosts/haenoe@saturn/services/traefik.nix
@@ -3,9 +3,6 @@
   virtualisation.oci-containers.containers.traefik = {
     autoStart = true;
     image = "traefik:v2.10";
-    cmd = [ "--api.insecure=true" "--providers.docker" ];
-    ports = [ "80:80" "8080:8080" ];
-    volumes = [ "/run/docker.sock:/var/run/docker.sock" ];
     cmd = [ 
       "--api.insecure=true" 
       "--providers.docker=true"
