a34073546631e369417146a0a4543be3d664f359
haenoe <programmierhaenoe@outlook.de>
2023-10-06 18:30:25 +0200

    feat(picom): add opacity and blur for alacritty

--- users/haenoe/picom.nix
+++ users/haenoe/picom.nix
@@ -1,3 +1,11 @@
 { ... }: {
-  services.picom.enable = true;
+  services.picom = { 
+    enable = true;
+    settings = {
+      # corner-radius = 16.0;
+      opacity-rule = [ "75:class_g = 'Alacritty'"  ];
+
+      blur = { method = "gaussian"; size = 10; deviation = 5.0; };
+    };
+  };
 }
