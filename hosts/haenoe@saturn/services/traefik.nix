c719bbddb5c41750860f3c04078300678cb10e88
haenoe <programmierhaenoe@outlook.de>
2023-10-06 18:34:37 +0200

    feat(traefik): add example whoami service

--- hosts/haenoe@saturn/services/traefik.nix
+++ hosts/haenoe@saturn/services/traefik.nix
@@ -25,4 +25,16 @@
     ports = [ "100.107.69.134:80:80" "100.107.69.134:443:443" "100.107.69.134:8080:8080" ];
     volumes = [ "/run/docker.sock:/var/run/docker.sock:ro" "/etc/traefik/letsencrypt:/letsencrypt" ];
   };
+  virtualisation.oci-containers.containers.whoami = {
+    autoStart = true;
+    image = "traefik/whoami";
+    dependsOn = [ "traefik" ];
+    extraOptions = [ 
+      "-ltraefik.enable=true"
+      "-ltraefik.http.routers.whoami.rule=Host(`whoami.saturn.haenoe.party`)"
+      "-ltraefik.http.routers.whoami.entrypoints=websecure"
+      "-ltraefik.http.routers.whoami.tls=true"
+      "-ltraefik.http.routers.whoami.tls.certresolver=cfresolver"
+    ];
+  };
 }
