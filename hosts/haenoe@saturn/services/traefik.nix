c000d4ee3fbb2147c10c06ae76c46e5d5e81ff2a
haenoe <programmierhaenoe@outlook.de>
2023-10-06 18:33:29 +0200

    feat(traefik): expose https endpoint

--- hosts/haenoe@saturn/services/traefik.nix
+++ hosts/haenoe@saturn/services/traefik.nix
@@ -6,5 +6,14 @@
     cmd = [ "--api.insecure=true" "--providers.docker" ];
     ports = [ "80:80" "8080:8080" ];
     volumes = [ "/run/docker.sock:/var/run/docker.sock" ];
+    cmd = [ 
+      "--api.insecure=true" 
+      "--providers.docker=true"
+      "--providers.docker.exposedbydefault=false"
+      "--entrypoints.websecure.address=:443"
+      "--entrypoints.web.address=:80"
+      "--entrypoints.web.http.redirections.entryPoint.to=websecure"
+      "--entrypoints.web.http.redirections.entryPoint.scheme=https"
+    ];
   };
 }
