6aef728909aa669b24a16652a4ef50b3632b8cc8
haenoe <programmierhaenoe@outlook.de>
2023-10-08 16:04:35 +0200

    feat(traefik): enable secure access to dashboard

--- hosts/haenoe@saturn/services/traefik.nix
+++ hosts/haenoe@saturn/services/traefik.nix
@@ -3,8 +3,8 @@
   virtualisation.oci-containers.containers.traefik = {
     autoStart = true;
     image = "traefik:v2.10";
-    cmd = [ 
-      "--api.insecure=true" 
+    cmd = [
+      "--api.dashboard=true"
       "--providers.docker=true"
       "--providers.docker.exposedbydefault=false"
       "--entrypoints.websecure.address=:443"
@@ -21,12 +21,21 @@
     };
     ports = [ "100.107.69.134:80:80" "100.107.69.134:443:443" "100.107.69.134:8080:8080" ];
     volumes = [ "/run/docker.sock:/var/run/docker.sock:ro" "/etc/traefik/letsencrypt:/letsencrypt" ];
+    extraOptions = [
+      "-ltraefik.enable=true"
+      "-ltraefik.http.routers.dashboard.rule=Host(`traefik.saturn.haenoe.party`)"
+      "-ltraefik.http.routers.dashboard.service=api@internal"
+      "-ltraefik.http.routers.dashboard.entrypoints=websecure"
+      "-ltraefik.http.routers.dashboard.tls=true"
+      "-ltraefik.http.routers.dashboard.tls.certresolver=cfresolver"
+    ];
   };
+
   virtualisation.oci-containers.containers.whoami = {
     autoStart = true;
     image = "traefik/whoami";
     dependsOn = [ "traefik" ];
-    extraOptions = [ 
+    extraOptions = [
       "-ltraefik.enable=true"
       "-ltraefik.http.routers.whoami.rule=Host(`whoami.saturn.haenoe.party`)"
       "-ltraefik.http.routers.whoami.entrypoints=websecure"
