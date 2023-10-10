ed8ce203f5613f2951b6016a383e11690eca5068
haenoe <programmierhaenoe@outlook.de>
2023-10-10 19:22:04 +0200

    feat(syncthing): add device and folder

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -1,4 +1,4 @@
-{ pkgs, hostInformation, ... }:
+{ hostInformation, ... }:
 {
   services.syncthing = {
     enable = true;
@@ -6,7 +6,17 @@
     dataDir = "/home/${hostInformation.mainUser}/syncthing";
     overrideDevices = true;
     overrideFolders = true;
-    settings.devices = { };
-    settings.folders = { };
+    settings.devices = {
+      alpha = {
+        id = "ZK5D52Y-I4LZ47T-GPLPGHX-YV2WDT6-YUQKJU4-WFLTHEA-XZTZ5QY-IKQQWQS";
+      };
+    };
+    settings.folders = {
+      aegis-backup = {
+        id = "n73tt-4phja";
+        path = "/home/haenoe/backups/aegis";
+        devices = [ "alpha" ];
+      };
+    };
   };
 }
