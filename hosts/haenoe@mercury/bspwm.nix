7b4e0d7c1ca7f78ed9e8229ee09a80ad3c9b826d
haenoe <programmierhaenoe@outlook.de>
2023-11-05 18:07:15 +0100

    feat(bspwm): add hotkeys to change layout

--- hosts/haenoe@mercury/bspwm.nix
+++ hosts/haenoe@mercury/bspwm.nix
@@ -38,6 +38,7 @@
           "alt + q" = "${pkgs.bspwm}/bin/bspc node -c";
           "alt + {_, shift +} + {a,o,e,u}" = "${pkgs.bspwm}/bin/bspc {desktop -f, node -d} {1-4}";
           "alt + {_, shift +} + {h,j,k,l}" = "${pkgs.bspwm}/bin/bspc node -{f,s} {west,south,north,east}";
+          "super + {t,shift + t,s,f}" = "${pkgs.bspwm}/bin/bspc node -t {tiled,pseudo_tiled,floating,fullscreen}";
         };
       };
     };
