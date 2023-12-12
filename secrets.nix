19fbb0bc2a82ce990f1a0268f2d7bcc5a916f6c3
haenoe <programmierhaenoe@outlook.de>
2023-12-12 20:15:35 +0100

    fix(traefik): add cloudflare_api_key secret

--- secrets.nix
+++ secrets.nix
@@ -1,7 +1,9 @@
 let
   mercury = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM4AzaVgNYLprK+gJ6fNmKTGIiwJ05NnX7S40BmZTCt8";
+  saturn = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtpGjRgMo3SIQlYxSByEhkoW0pTpwxxSq91cqWqMlpq";
   systems = [
     mercury
+    saturn
   ];
   haenoe = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE5laWkCjzbloX88KuPDJprh9AkHAFnPUGfEuTZyxjtp";
   users = [
@@ -9,5 +11,6 @@ let
   ];
 in
 {
-  "hosts/haenoe@mercury/repository-key.age".publicKeys = systems ++ users;
+  "hosts/haenoe@mercury/repository-key.age".publicKeys = [ mercury ] ++ users;
+  "hosts/haenoe@saturn/cloudflare-api-key.age".publicKeys = [ saturn ] ++ users;
 }
